af323022df4752fb6230603785c435dcd0122a0b
s4mwich <danielsam0311@gmail.com>
2026-02-18 16:28:25 -0500

    Initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "tailwindcss";
+
+export default {
+    theme: {
+        extend: {
+            colors: {
+                mainBackgroundColor: "#0D1117",
+                columnBackgroundColor: "#161C22",
+            },
+        },
+    },
+} satisfies Config;
